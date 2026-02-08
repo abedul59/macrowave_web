92bda0f191139a082cb1cd5c6c5052261ad071c9
pyfbsdk59 <pyfbsdk59@gmail.com>
2026-02-08 23:31:51 +0800

    4th

--- config/urls.py
+++ config/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
 from django.urls import path
-from . import views
+from dashboard import views  # <--- 修正這行：從 'dashboard' app 匯入，而不是從 '.'
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('scrape/', views.manual_scrape, name='manual_scrape'), # 新增這行
+    path('scrape/', views.manual_scrape, name='manual_scrape'),
 ]
